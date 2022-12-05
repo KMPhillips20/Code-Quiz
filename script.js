5a840877c026922829f05ee5e9f5e49913a12d20
Kyle <kylemphillips65@yahoo.com>
2022-12-05 17:08:58 -0500

    added buttons to my page

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ var cursor = 0;
 var displayQuestion = function () {
     for (var question of questions) {
         console.log(question);
+        if (question.dataset.index != cursor) {
+            question.style.display = 'none';
+        } else {
+            question.style.display = 'block';
+        }
     }
 };
 
-var advance = function() {
-    if (cursor < questions.length -1) {
+var advance = function(event) {
+    var element = event.target;
+    console.log(element);
+
+    if (element.matches('.question button')) {
+        if (cursor < questions.length -1) {
         cursor++;
     }
     displayQuestion();
+}
 };
+
+document.addEventListener('click', advance);
 
 displayQuestion();

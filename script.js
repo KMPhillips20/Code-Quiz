d89fb2bb0b1f6d59d789b9489ac4b128f5ef3c94
Kyle <kylemphillips65@yahoo.com>
2022-12-07 16:53:34 -0500

    debugged js to get time and questions to display in my console log

--- script.js
+++ script.js
@@ -1,52 +1,30 @@
-var questions = document.querySelectorAll('.question');
-var cursor = 0;
-var correctAnswers = ["0", "B", "D", "A","B","B"];
-const startingMinutes=1;
-var endScreen= document.querySelector('#endScreen');
-var highScoreScreen= document.querySelector('#highScoreScreen');
+var question = document.querySelectorAll('#question');
+var container = document.querySelector('#container');
+var startBTN = document.querySelector('#startBTN');
+var start = document.querySelector('#start');
+var end= document.querySelector('#end');
+var score = document.querySelector('#score');
+var name = document.querySelector('#name')
+var highScore = document.querySelector('#highScore');
+// var cursor = 0;
+// var correctAnswers = ["0", "B", "D", "A","B","B"];
 
 
 
 // Timer
-let time = startingMinutes * 59;
-const countdownEl = document.getElementById('countdown');
-setInterval(updateCountdown, 1000);
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
-}
-
-
-// buttons
-var displayQuestion = function () {
-    for (var question of questions) {
-         if (question.dataset.index != cursor) {
-            question.style.display = 'none';
-        } else {
-            question.style.display = 'block';
-        }
-    }
+var startTimer = function(){
+    console.log('time started');
 };
 
-var advance = function(event) {
-    var element = event.target;
-    if (element.matches('.question button')) {
-      var answer = element.dataset.choice === correctAnswers[cursor];
-       console.log(answer)
-    
-        if (cursor < questions.length ) {
-         cursor++;
-        }
-         displayQuestion();
-    }
+var displayQuestion = function() {
+    console.log('displayed question');
 };
 
+startBTN.addEventListener('click', function() {
+    startTimer();
+    displayQuestion();
 
-document.addEventListener('click', advance);
+});
 
 
 
-
-displayQuestion();

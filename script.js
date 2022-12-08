1f1c29b5f9afa0d74d530b7f351e8f27ade4356d
Kyle <kylemphillips65@yahoo.com>
2022-12-07 19:17:21 -0500

    deBugged js to make things work better

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var question = document.querySelectorAll('#question');
+var question = document.querySelector('#question');
 var container = document.querySelector('#container');
 var startBTN = document.querySelector('#startBTN');
+var button = document.querySelector('#button')
 var start = document.querySelector('#start');
 var end= document.querySelector('#end');
 var score = document.querySelector('#score');
@@ -9,22 +10,48 @@ var highScore = document.querySelector('#highScore');
 // var cursor = 0;
 // var correctAnswers = ["0", "B", "D", "A","B","B"];
 
+var index = 0;
+
+var questions = [0, 1];
 
 
-// Timer
 var startTimer = function(){
     console.log('time started');
 };
 
 var displayQuestion = function() {
-    console.log('displayed question');
+    console.log('display questions');
 };
+
+var nextQuestion = function() {
+    var correct = true;
+    index++;
+    if (correct) {
+        console.log("Correct");
+    } else {
+        console.log('Wrong');
+    }
+    displayQuestion();
+};
+
 
 startBTN.addEventListener('click', function() {
     startTimer();
     displayQuestion();
-
+    var sections = document.querySelectorAll("section");
+    console.log(sections);
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+    question.style.display = "block";
 });
 
+button.addEventListener('click', function (event) {
+    var element = event.target;
 
+    if (element.matches('button')) {
+        nextQuestion();
+    }
+
+});
 
